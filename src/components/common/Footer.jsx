f72047b0c95a992e2b8a74b360d09456fb0f23e9
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-11-09 14:50:20 -0300

    empece con maquetado del footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <section className='py-5 bg-dark text-light'>
+            <Container>
+                <Row>
+                    <Col>
+                    <p>logo</p>
+                    </Col>
+                    <Col>
+                    <p>Home</p>
+                    <p>Acerca de nosotros</p>
+                    <p>Encontranos en</p>
+                    <p>Contactanos</p>
+                    </Col>
+                    <Col>
+                    <p>Seguinos en nuestras redes</p>
+                    <i class="bi bi-facebook fs-4 me-3"></i>
+                    <i class="bi bi-whatsapp fs-4 me-3"></i>
+                    <i class="bi bi-instagram fs-4"></i>
+                    </Col>
+                </Row>
+            </Container>
+                <p className='text-center mt-4'>&copy; Todos los derechos reservados</p>
+        </section>
     );
 };
 
