e17cf6b07fa3289a01f340fa383785bf0f19e335
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-11-09 16:43:10 -0300

    agregue logo

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
         <section className='py-5 bg-dark text-light'>
             <Container>
                 <Row>
-                    <Col>
-                    <p>logo</p>
+                    <Col md={4}>
+                    <img src='https://trello.com/1/cards/636987e596b2de018dfa4d9c/attachments/636bbf6a4fc778028d53781f/previews/636bbf6a4fc778028d537889/download/Logo_Sham.png' alt='logo sham' className='w-50'></img>
                     </Col>
-                    <Col>
+                    <Col md={4}>
                     <p>Home</p>
                     <p>Acerca de nosotros</p>
                     <p>Encontranos en</p>
                     <p>Contactanos</p>
                     </Col>
-                    <Col>
+                    <Col md={4}>
                     <p>Seguinos en nuestras redes</p>
                     <i class="bi bi-facebook fs-4 me-3"></i>
                     <i class="bi bi-whatsapp fs-4 me-3"></i>
