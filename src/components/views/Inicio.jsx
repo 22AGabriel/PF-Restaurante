c00f6fd691906601c50acb1dfb8e0489175a3dc8
Javier Uncos <javiersrfc30@gmail.com>
2022-11-10 23:31:33 -0300

    cambios en la imagen de portada en dispositivos moviles

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,73 +1,74 @@
 import React from "react";
 import "./Inicio.css";
 import { Row, Col, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Inicio = () => {
   return (
     <div className="mainSection">
       <div className=" text-light portada ">
         <div className="subPortada">
-          <h1 className="display-3">Comidas Arabes</h1>
+          <h1 className="display-1">Comidas Arabes</h1>
         </div>
       </div>
       <div className=" mb-5 container-fluid">
         <div className="container">
           <Row className="mx-auto menuCard">
             <Col md={4}>
-              <Card style={{ width: "18rem" }} className="cardCirculo ms-5">
+              <Card  className="cardCirculo ms-5">
                 <div className="imgCardP">
                   <div>
-                   <button className="btnMenu"><i class="bi bi-file-person text-light display-4 iconMenu"></i></button>
+                  <Button as={Link} to="/login" className="btnMenu"><i class="bi bi-people-fill text-light display-4 iconMenu"></i></Button>
                   </div>
                   <Card.Img variant="top" src="" />
                 </div>
                 <Card.Body className="bodyCard">
-                  <Card.Title> Sobre Nosotros</Card.Title>
-                  <Button variant="warning">Go somewhere</Button>
+                  <Card.Title className="text-light my-3"> login</Card.Title>
+                  <Link variant="warning" to="/nosotros" className="btn btn-warning">Ver mas</Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card style={{ width: "18rem" }} className="cardCirculo ms-5">
+              <Card className="cardCirculo ms-5">
                 <div className="imgCardP">
                  <div>
-                   <button className="btnMenu"><i class="bi bi-book text-light display-4 iconMenu"></i></button>
+                   <Button as={Link} to="/nosotros" className="btnMenu"><i class="bi bi-book text-light display-4 iconMenu"></i></Button>
                   </div>
                 </div>
                 <Card.Body className="bodyCard">
-                  <Card.Title>Categorias</Card.Title>
-                  <Button variant="warning">Go somewhere</Button>
+                  <Card.Title className="text-light my-3">Nosotros</Card.Title>
+                  <Link variant="warning" className="btn btn-warning">Ver mas</Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card style={{ width: "18rem" }} className="cardCirculo ms-3">
+              <Card  className="cardCirculo ms-3">
                 <div className="imgCardP">
                 <div>
-                   <button className="btnMenu"><i class="bi bi-cart text-light display-4 iconMenu"></i></button>
+                <Button as={Link} className="btnMenu"><i class="bi bi-cart text-light display-4 iconMenu"></i></Button>
                   </div>
                 </div>
                 <Card.Body className="bodyCard">
-                  <Card.Title>Pedidos</Card.Title>
-                  <Button variant="warning">Go somewhere</Button>
+                  <Card.Title className="text-light my-3">Pedidos</Card.Title>
+                  <Link variant="warning" className="btn btn-warning">Ver mas</Link>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
         </div>
       </div>
-      <div className="portada2 container-fluid">
+      <div className="portada2 ">
         <div className="divPortada2">
-          <h1 className="display-1 text-light">Descubri nuestras especialidades</h1>
+          <h1 className="display-2 text-light">Descubri nuestras especialidades</h1>
           <h2 className="display-1">Nombre</h2>
         </div>
       </div>
       <div className="container">
-        <h1 className="text-center text-dark display-3 my-5">Productos</h1>
+        <h1 className="text-center text-light display-3 my-5">Productos</h1>
         <div>
           <Row>
             <Col md={4}>
-              <Card className="mb-3">
+              <Card className="mb-3 CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -75,15 +76,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card className="mb-3">
+              <Card className="mb-3 CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -91,15 +91,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -107,15 +106,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -123,15 +121,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -139,15 +136,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -155,15 +151,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -171,15 +166,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -187,15 +181,14 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card>
+              <Card className="CardPP">
                 <Card.Img
                   variant="top"
                   src="https://www.serargentino.com/public/images/2020/02/comida-arabe-773x458.jpeg"
@@ -203,19 +196,18 @@ const Inicio = () => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                   Precio: $500
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
         </div>
       </div>
-      <div className="portada2 container-fluid mt-5">
+      <div className="portada2  mt-5">
         <div className="divPortada2">
-          <h1 className="display-1 text-light">Descubri nuestras especialidades</h1>
+          <h1 className="display-1 text-light">Los mejores Precios</h1>
            <h2 className="tituloCel display-3">Haz tu pedido</h2>
         </div>
       </div>
