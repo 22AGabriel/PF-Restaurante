bce8dd57428126c8b4f2e4646cfb386992167e73
Javier Uncos <javiersrfc30@gmail.com>
2022-11-11 12:59:31 -0300

    agregamos modificaciones al menu de botones  de la portada

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Inicio.css";
+import "../../Inicio.css";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
@@ -8,49 +8,65 @@ const Inicio = () => {
     <div className="mainSection">
       <div className=" text-light portada ">
         <div className="subPortada">
-          <h1 className="display-1">Comidas Arabes</h1>
+          <h1 className="display-1">Shamy comidas Arabes</h1>
         </div>
       </div>
       <div className=" mb-5 container-fluid">
         <div className="container">
           <Row className="mx-auto menuCard">
             <Col md={4}>
-              <Card  className="cardCirculo ms-5">
+              <Card className="cardCirculo ms-5">
                 <div className="imgCardP">
                   <div>
-                  <Button as={Link} to="/login" className="btnMenu"><i class="bi bi-people-fill text-light display-4 iconMenu"></i></Button>
+                    <Button as={Link} to="/login" className="btnMenu">
+                      <i class="bi bi-people-fill text-light display-4 iconMenu"></i>
+                    </Button>
                   </div>
                   <Card.Img variant="top" src="" />
                 </div>
-                <Card.Body className="bodyCard">
+                <Card.Body className="bodyCard text-light">
                   <Card.Title className="text-light my-3"> login</Card.Title>
-                  <Link variant="warning" to="/nosotros" className="btn btn-warning">Ver mas</Link>
+                  <Link
+                    variant="warning"
+                    to="/nosotros"
+                    className="btn btn-warning"
+                  >
+                    Ver mas
+                  </Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
               <Card className="cardCirculo ms-5">
                 <div className="imgCardP">
-                 <div>
-                   <Button as={Link} to="/nosotros" className="btnMenu"><i class="bi bi-book text-light display-4 iconMenu"></i></Button>
+                  <div>
+                    <Button as={Link} to="/nosotros" className="btnMenu">
+                      <i class="bi bi-book text-light display-4 iconMenu"></i>
+                    </Button>
                   </div>
                 </div>
-                <Card.Body className="bodyCard">
+                <Card.Body className="bodyCard text-light">
                   <Card.Title className="text-light my-3">Nosotros</Card.Title>
-                  <Link variant="warning" className="btn btn-warning">Ver mas</Link>
+                  <Link variant="warning" className="btn btn-warning">
+                    Ver mas
+                  </Link>
                 </Card.Body>
               </Card>
             </Col>
             <Col md={4}>
-              <Card  className="cardCirculo ms-3">
+              <Card className="cardCirculo ms-3">
                 <div className="imgCardP">
-                <div>
-                <Button as={Link} className="btnMenu"><i class="bi bi-cart text-light display-4 iconMenu"></i></Button>
+                  <div>
+                    <Button as={Link} className="btnMenu">
+                      <i class="bi bi-cart text-light display-4 iconMenu"></i>
+                    </Button>
                   </div>
                 </div>
-                <Card.Body className="bodyCard">
+                <Card.Body className="bodyCard text-light">
                   <Card.Title className="text-light my-3">Pedidos</Card.Title>
-                  <Link variant="warning" className="btn btn-warning">Ver mas</Link>
+                  <Link variant="warning" className="btn btn-warning">
+                    Ver mas
+                  </Link>
                 </Card.Body>
               </Card>
             </Col>
@@ -59,7 +75,9 @@ const Inicio = () => {
       </div>
       <div className="portada2 ">
         <div className="divPortada2">
-          <h1 className="display-2 text-light">Descubri nuestras especialidades</h1>
+          <h1 className="display-2 text-light">
+            Descubri nuestras especialidades
+          </h1>
           <h2 className="display-1">Nombre</h2>
         </div>
       </div>
@@ -75,9 +93,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -90,9 +106,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                  Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -105,9 +119,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="danger">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -120,9 +132,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -135,9 +145,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -150,9 +158,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                  Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="success">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -165,9 +171,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -180,9 +184,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -195,9 +197,7 @@ const Inicio = () => {
                 />
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                   Precio: $500
-                  </Card.Text>
+                  <Card.Text>Precio: $500</Card.Text>
                   <Button variant="warning">Ver mas</Button>
                 </Card.Body>
               </Card>
@@ -208,7 +208,7 @@ const Inicio = () => {
       <div className="portada2  mt-5">
         <div className="divPortada2">
           <h1 className="display-1 text-light">Los mejores Precios</h1>
-           <h2 className="tituloCel display-3">Haz tu pedido</h2>
+          <h2 className="tituloCel display-3">Haz tu pedido</h2>
         </div>
       </div>
     </div>
