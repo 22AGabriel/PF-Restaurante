fdd0d352d6d42c54840133c017f3b9ec1b1063ae
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-11-12 19:21:35 -0300

    se mejora el maquetado agrego margin donde corresponde

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -14,63 +14,61 @@ const DetalleProducto = () => {
 
   return (
     <>
-      <Container className="d-flex justify-content-center">
+      <Container>
         <Card>
           <Row>
             <Col md={4}>
-              <img
+              <Card.Img
                 className="w-100"
                 src="https://images.pexels.com/photos/6275163/pexels-photo-6275163.jpeg?auto=compress&cs=tinysrgb&w=600"
                 alt="salsa arabe"
-              ></img>
+              ></Card.Img>
             </Col>
             <Col md={8}>
-              <Card className="w-75 my-5 rounded-2">
-                <Card.Body>
-                  <Card.Title className="fs-2 w-bold mt-3 d-flex justify-content-center">
-                    Nombre
-                  </Card.Title>
-                  <hr />
-                  <Card.Text className="fs-5">
-                    Detalle:
-                    <br></br>
-                    <p>
-                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                      Dolor dolore sequi incidunt aliquam sed quam porro ipsam
-                      dolorum voluptate dolores?
-                    </p>
-                  </Card.Text>
-                  <Card.Text className="fs-5">Precio: $</Card.Text>
-                  <Card className="d-flex p-2 shadow p-3 mb-5 bg-body rounded">
-                    <Form className="d-flex flex-row">
-                      <Form.Group>
-                        <Form.Label className="fs-5">Cantidad</Form.Label>
-                        <div>
-                          <h3>{numeritos}</h3>
-                          <Button
-                            className="me-2"
-                            variant="outline-primary"
-                            onClick={() => setNumeritos(numeritos + 1)}
-                          >
-                            +1
-                          </Button>
-                          <Button
-                            className=""
-                            variant="outline-primary"
-                            onClick={() => setNumeritos(numeritos - 1)}
-                          >
-                            -1
-                          </Button>
-                        </div>
-                      </Form.Group>
-                    </Form>
-                  </Card>{" "}
-                  <div className="d-flex justify-content-end">
-                    <i class="bi bi-bag-check-fill fs-1 mx-3"></i>
-                    <i class="bi bi-cart-fill fs-1 mx-3"></i>
-                  </div>
-                </Card.Body>
-              </Card>
+              <Card.Body>
+                <Card.Title className="fs-2 w-bold mt-3 d-flex justify-content-center">
+                  Nombre
+                </Card.Title>
+                <hr />
+                <Card.Text className="fs-5">
+                  Detalle:
+                  <br></br>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Dolor dolore sequi incidunt aliquam sed quam porro ipsam
+                    dolorum voluptate dolores?
+                  </p>
+                </Card.Text>
+                <Card.Text className="fs-5">Precio: $</Card.Text>
+                <Card className="d-flex p-2 shadow p-3 mb-5 bg-body rounded">
+                  <Form>
+                    <Form.Group>
+                      <Form.Label className="fs-5">Cantidad</Form.Label>
+                      <div>
+                        <h3>{numeritos}</h3>
+                        <Button
+                          className="me-2"
+                          variant="outline-primary"
+                          onClick={() => setNumeritos(numeritos + 1)}
+                        >
+                          +1
+                        </Button>
+                        <Button
+                          className=""
+                          variant="outline-primary"
+                          onClick={() => setNumeritos(numeritos - 1)}
+                        >
+                          -1
+                        </Button>
+                      </div>
+                    </Form.Group>
+                  </Form>
+                </Card>
+                <article className="d-flex justify-content-end">
+                  <i class="bi bi-bag-check-fill fs-1 mx-3"></i>
+                  <i class="bi bi-cart-fill fs-1 mx-3"></i>
+                </article>
+              </Card.Body>
             </Col>
           </Row>
         </Card>
