310a449b56f75e35060015b3a604ec448be8999a
Jochyd <mail>
2022-11-12 21:54:19 -0300

    comence el nav con offcanvas

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
-
-const NavBar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    );
-};
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavLink } from 'react-router-dom';
+const  NavBar = () => {
+  return (
+    <>
+    {[false].map((expand) => (
+      <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img src="" alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}/>
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavLink href="#action1">Home</NavLink>
+                <NavLink href="#action2">Link</NavLink>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    ))}
+  </>
+);
+}
 
 export default NavBar;
