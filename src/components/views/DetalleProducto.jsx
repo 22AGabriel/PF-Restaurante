9894ed90db5b978f04716bec345af0238e1fb77e
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-11-13 15:05:00 -0300

    se agregan colores a la pagina y se mejora la card que contiene el contador

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -7,22 +7,22 @@ const DetalleProducto = () => {
   return (
     <>
       <Container>
-        <Card>
+        <Card className="mt-4">
           <Row>
-            <Col md={4}>
+            <Col md={4} className="bg-rojo3">
               <Card.Img
                 src="https://images.pexels.com/photos/6252729/pexels-photo-6252729.jpeg?auto=compress&cs=tinysrgb&w=600"
                 className="w-100 mt-5 m-2"
                 alt="salsa arabe"
               ></Card.Img>
             </Col>
-            <Col md={8}>
+            <Col md={8} className="bg-rojo3">
               <Card.Body>
-                <Card.Title className="fs-2 w-bold mt-3 d-flex justify-content-center">
+                <Card.Title className="fs-2 w-bold mt-3 d-flex justify-content-center text-dark">
                   Nombre
                 </Card.Title>
                 <hr />
-                <Card.Text className="fs-5">
+                <Card.Text className="fs-5 text-dark">
                   Detalle:
                   <br></br>
                   <p>
@@ -31,32 +31,33 @@ const DetalleProducto = () => {
                     dolorum voluptate dolores?
                   </p>
                 </Card.Text>
-                <Card.Text className="fs-5">Precio: $</Card.Text>
-                <Card className="d-flex p-2 shadow p-3 mb-5 bg-body rounded">
+                <Card.Text className="fs-5 text-dark">Precio: $</Card.Text>
+                <Card className="shadow bg-rojo4 text-dark p-2 rounded">
                   <Form>
                     <Form.Group>
-                      <Form.Label className="fs-5">Cantidad</Form.Label>
-                      <div>
+                      <div className="d-flex p-2">
+                        <Form.Label className="fs-5 p-1 me-2">
+                          Cantidad:
+                        </Form.Label>
+
+                        <Button
+                          className="me-2 bg-rojo3 p-1"
+                          onClick={() => setNumeritos(numeritos)}
+                        >
+                          +
+                        </Button>
                         <h3>{numeritos}</h3>
                         <Button
-                          className="me-2"
-                          variant="outline-primary"
-                          onClick={() => setNumeritos(numeritos + 1)}
+                          className="me-2 bg-rojo3 p-1"
+                          onClick={() => setNumeritos(numeritos)}
                         >
-                          +1
-                        </Button>
-                        <Button
-                          className=""
-                          variant="outline-primary"
-                          onClick={() => setNumeritos(numeritos - 1)}
-                        >
-                          -1
+                          -
                         </Button>
                       </div>
                     </Form.Group>
                   </Form>
                 </Card>
-                <article className="d-flex justify-content-end">
+                <article className="d-flex justify-content-end ">
                   <i class="bi bi-bag-check-fill fs-1 mx-3"></i>
                   <i class="bi bi-cart-fill fs-1 mx-3"></i>
                 </article>
