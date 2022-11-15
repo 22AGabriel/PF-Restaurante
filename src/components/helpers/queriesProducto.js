f546b80e37ed0cdac23a5138f676dc22b37d7be2
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-15 17:26:13 -0300

    agregué la peticion get para obtener un producto por su id

--- src/components/helpers/queriesProducto.js
+++ src/components/helpers/queriesProducto.js
@@ -1,8 +1,5 @@
 const URL = process.env.REACT_APP_API_PRODUCTOS;
 
-
-
-
 export const crearProducto = async(producto)=>{
     try {
         const nuevoProducto = await fetch(URL,{
@@ -16,5 +13,19 @@ export const crearProducto = async(producto)=>{
     } catch (error) {
         console.log(error)
         return false
+    }
+}
+
+export const obtenerProducto = async(id)=> {
+    try{
+        const respuesta = await fetch(URL+`/${id}`);
+        const producto = {
+            dato: await respuesta.json(),
+            status: respuesta.status
+        }
+        return producto;
+    } catch(error){
+        console.log(error);
+        return false;
     }
 }
