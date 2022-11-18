9df8ac14a4c0f55000969c8377438d4d73bfe14b
Javier Uncos <javiersrfc30@gmail.com>
2022-11-17 22:28:22 -0300

    agregamos en la funcion agregar un map para aumentar la cantidad de producto

--- src/components/helpers/queriesCarrito.js
+++ src/components/helpers/queriesCarrito.js
@@ -3,8 +3,18 @@ import { editarUsuario } from "./queriesUsuario";
 export const editarCarrito = (producto)=>{
     let usuario = JSON.parse(localStorage.getItem("usuarioIniciado"))
     let carrito = usuario.carrito;
+    const existe = carrito.some(producto=> producto.id === producto.id)
+
+    if(existe){
+        const prod = carrito.map((prod)=>{
+            if(prod.id === producto.id){
+                producto.cantidad++
+            }
+        })
+    }
     carrito.push(producto)
     usuario.carrito = carrito
-    editarUsuario(usuario.id,usuario)
-    localStorage.setItem('usuarioIniciado', JSON.stringify(usuario))
+     editarUsuario(usuario.id,usuario)
+     localStorage.setItem('usuarioIniciado', JSON.stringify(usuario))
+    
 }
