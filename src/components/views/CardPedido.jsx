57f10eadb2a7c11b4f2cde76d48559730e843de3
Javier Uncos <javiersrfc30@gmail.com>
2022-11-22 20:34:32 -0300

    merge de dev y estilos nuevos a la cards Pedidos

--- src/components/views/CardPedido.jsx
+++ src/components/views/CardPedido.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
-import { Button, Card, Col, Row } from "react-bootstrap";
-import Pedidos from "./Pedidos";
+import { Button, Container } from "react-bootstrap";
 
 const CardPedido = ({ pedido }) => {
   return (
     <>
-      <div className="w-100 mx-auto container text-dark ">
-        <div className="row bg-rojo3 text-dark mt-5 contenerdorPedidos">
-          <div className="d-flex justify-content-between mt-1">
-            <h3 className="text-light">pedido</h3>
-            <h5 className="mt-2">{pedido.fecha}</h5>
+      <Container className="bajarFooter">
+        <div className="row bg-rojo3 text-dark mt-4 rounded">
+          <div className="d-flex justify-content-end mt-2">
+            <h6>{pedido.fecha}</h6>
           </div>
-          <hr className="text-dark" />
+          <hr />
           <div className="col-12 col-md-6">
             <ul>
               {pedido.productos.map((item,indice) => (
@@ -56,9 +54,9 @@ const CardPedido = ({ pedido }) => {
             ))}
           </div>
         </div>
-      </div>
+      </Container>
     </>
   );
 };
 
-export default CardPedido;
+export default CardPedido;
--- src/components/views/Pedidos.jsx
+++ src/components/views/Pedidos.jsx
@@ -5,9 +5,7 @@ import CardPedido from "./CardPedido";
 const Pedidos = ({ usuarioLogueado }) => {
   return (
     <div className="bajarFooter mt-5">
-      <h1 className="display-4 text-center">El Sham</h1>
-      <hr />
-      <h3 className="display-6 text-center">Tus pedidos</h3>
+      <h1 className="display-4 text-center">Tus pedidos</h1>
       <Container>
         {usuarioLogueado.pedidos.map((item, indice) => (
           <CardPedido pedido={item} key={indice}></CardPedido>
