aed33df4a921e049fcc3645855fc1a59c581aa44
Jochyd <mail>
2022-11-23 22:22:50 -0300

    agregue el path para enviar al 404

--- src/components/routes/RutasAdmin.jsx
+++ src/components/routes/RutasAdmin.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Admin from '../views/Admin';
 import AgregarProducto from '../views/producto/AgregarProducto';
 import EditarProducto from '../views/producto/EditarProducto';
+import Error404 from '../views/Error404'
 
 const RutasAdmin = () => {
     return (
@@ -11,6 +12,7 @@ const RutasAdmin = () => {
         <Route exact path="/" element={<Admin/>}/>
         <Route exact path="/agregar" element={<AgregarProducto/>}/>
         <Route exact path="/editar/:id" element={<EditarProducto/>}/>
+        <Route exact path="*" element={<Error404></Error404>}/>
         </Routes>
         </>
     );
--- src/components/routes/RutasUsuarios.jsx
+++ src/components/routes/RutasUsuarios.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Pedidos from "../views/Pedidos";
+import Error404 from "../views/Error404";
 
 const RutasUsuarios = ({usuarioLogueado}) => {
     return (
         <>
         <Routes>
         <Route exact path="/pedidos" element={<Pedidos usuarioLogueado={usuarioLogueado}></Pedidos>}/>
+        <Route exact path="*" element={<Error404></Error404>}/>
         </Routes>
         </>
     );
