5ad8efeec34a63fcd0e4b9c2c37225b0821e2750
22AGabriel <107433737+22AGabriel@users.noreply.github.com>
2022-11-27 20:11:31 -0300

    cambios estéticos en la card

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -1,15 +1,9 @@
-import {
-  Card,
-  Container,
-  Button,
-  Breadcrumb,
-} from "react-bootstrap";
+import {Card, Container, Button, Breadcrumb} from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { obtenerProducto } from "../helpers/queriesProducto";
 import { Link, useParams } from "react-router-dom";
 import { editarUsuario } from "../helpers/queriesUsuario";
 import "../../css/app.css";
-
 
 const DetalleProducto = ({carrito, setCarrito, usuarioLogueado}) => {
   const [producto, setProducto] = useState({});
@@ -39,9 +33,9 @@ const DetalleProducto = ({carrito, setCarrito, usuarioLogueado}) => {
 
   return (
     <>
-      <Container className="my-5">
+      <Container className="my-5 bajarFooter">
         <Breadcrumb>
-        <Breadcrumb.Item><Link className="text-rojo2" to={"/"}>Inicio</Link></Breadcrumb.Item>
+          <Breadcrumb.Item><Link className="text-rojo2" to={"/"}>Inicio</Link></Breadcrumb.Item>
           <Breadcrumb.Item><Link className="text-rojo2" to={"*"}>{producto.categoria}</Link></Breadcrumb.Item>
           <Breadcrumb.Item active>{producto.nombreProducto}</Breadcrumb.Item>
         </Breadcrumb>
@@ -50,14 +44,14 @@ const DetalleProducto = ({carrito, setCarrito, usuarioLogueado}) => {
         </div>
         <div className="my-3 d-flex flex-column detalleProductoDiv">
           <div className="divImgDetalle">
-            <img className="w-100  rounded " src={producto.imagen} alt="" />
+            <img className="w-100 rounded" src={producto.imagen} alt={producto.nombreProducto} />
           </div>
-          <Card>
-            <Card.Body className="text-dark bg-rojo3">
+          <Card className="border-0">
+            <Card.Body className="text-dark bg-rojo3 rounded">
               <div className="d-flex justify-content-between">
                 <Card.Text>Precio: ${producto.precio}</Card.Text>
                 <Button
-                  className="bg-rojo2 text-light buttonCard "
+                  className="bg-rojo2 text-dark borde-rojo3"
                   onClick={() => {
                     editarCarrito(producto);
                   }}
